b8fc7f915762e05d837a92db1037b11eded0c27a
mbonikevin <k.mbonimpay@alustudent.com>
2025-04-02 09:15:50 +0000

    3

---
+++ 0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# coping a file
+
+file {'copy_file':
+  ensure => file,
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
+  path   => '/var/www/html/wp-includes/class-wp-locale.phpp',}
